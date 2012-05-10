4d256af991434a4e217993826071bdadfe010c90
Grzegorz Niewisiewicz <grzegorz.niewisiewicz@gmail.com>
2012-05-10 22:01:15 +0200

    Refactor the engine and the UI
    - add one-line comments to functions
    - allow more than two programs
    - mark programs with colors
    - add stepping mode
    - add reboot

--- corewar.js
+++ corewar.js
@@ -1,3 +1,4 @@
+// Return names of own properties of an object.
 Object.prototype.getOwnProperties = function () {
     var result = [];
     for (var i in this) {
@@ -9,91 +10,122 @@ Object.prototype.getOwnProperties = function () {
 };
 
 
-function Process(queue) {
+// A MARS process.
+function Process(owner, queue) {
+    this.owner = owner;
     this.queue = queue;
 }
 
+// Test if a process has a thread at an offset.
+Process.prototype.hasThreadAt = function (offset) {
+    return this.queue.indexOf(offset) !== -1;
+};
 
+
+// A MARS address.
 function Address(mode, value) {
     this.mode = mode;
     this.value = value;
 }
 
+// Test if addresses are equal.
 Address.prototype.isEqual = function (address) {
-    return this.mode == address.mode && this.value == address.value;
+    return this.mode === address.mode
+        && this.value === address.value;
 };
 
+// Clone an address.
 Address.prototype.clone = function () {
     return new Address(this.mode, this.value);
 };
 
-
+// Convert an address to a string.
 Address.prototype.toString = function () {
     return this.mode + this.value;
 };
 
 
-function Cell(opcode, a, b) {
+// A MARS instruction.
+function Instruction(opcode, a, b) {
     this.opcode = opcode;
     this.a = a;
     this.b = b;
 }
 
-Cell.prototype.isEqual = function (cell) {
-    return this.opcode === cell.opcode && this.a.isEqual(cell.a) && this.b.isEqual(cell.b);
+// Test if instructions are equal.
+Instruction.prototype.isEqual = function (instruction) {
+    return this.opcode === instruction.opcode
+        && this.a.isEqual(instruction.a)
+        && this.b.isEqual(instruction.b);
 };
 
-Cell.prototype.clone = function () {
-    return new Cell(this.opcode, this.a.clone(), this.b.clone());
+// Clone an instruction.
+Instruction.prototype.clone = function () {
+    return new Instruction(this.opcode, this.a.clone(), this.b.clone());
 };
 
-Cell.prototype.toString = function () {
+// Convert an instruction to a string.
+Instruction.prototype.toString = function () {
     return this.opcode + " " + this.a.toString() + "," + this.b.toString();
 };
 
 
+// MARS (Memory Array Redcode Simulator)
 function Mars(coresize) {
     this.core = new Array(coresize);
     this.reboot();
 }
 
+// Set all memory to DAT 0,0 and remove all processes.
 Mars.prototype.reboot = function () {
     for (var i = 0; i < this.core.length; i++) {
-        this.core[i] = new Cell("DAT", new Address("", 0), new Address("", 0));
+        this.core[i] = new Instruction(
+            "DAT",
+            new Address("", 0),
+            new Address("", 0)
+        );
     }
     this.processes = [];
 };
 
-// Compilation
-
-Mars.prototype.compileLine = function (line) {
+// Return a regular expression matching Redcode instructions.
+Mars.prototype.getInstructionRegex = function () {
     var opcodes = this.getOpcodes().join("|");
     var modes = this.getModes().join("|");
-    var regex = "^\\s*(" + opcodes + ")\\s+(" + modes + ")(-?\\d+)\\s*,\\s*(" + modes + ")(-?\\d+)\\s*\\n?$";
-    var matches = line.match(regex);
-    if (!matches) {
-        throw "invalid Redcode instruction: " + line;
-    }
-    return new Cell(matches[1],
-        new Address(matches[2], parseInt(matches[3], 10)),
-        new Address(matches[4], parseInt(matches[5], 10)));
+    var regex = "^\\s*(" + opcodes + ")\\s+(" + modes
+        + ")(-?\\d+)\\s*,\\s*(" + modes + ")(-?\\d+)\\s*\\n?$";
+    return regex;
 };
 
-Mars.prototype.compileLines = function (input) {
-    var lines = input.split("\n");
-    var opcodes = [];
+// Compile a Redcode instruction into a MARS instruction.
+Mars.prototype.compileInstruction = function (line) {
+    var regex = this.getInstructionRegex();
+    var matches = line.match(regex);
+    if (!matches) {
+        throw "cannot compile " + line;
+    }
+    return new Instruction(
+        matches[1],
+        new Address(matches[2], parseInt(matches[3], 10)),
+        new Address(matches[4], parseInt(matches[5], 10))
+    );
+};
+
+// Compile a Redcode program into an array of MARS instructions.
+Mars.prototype.compileProgram = function (program) {
+    var lines = program.split("\n");
+    var instructions = [];
     for (var i = 0; i < lines.length; i++) {
         var line = lines[i];
         if (line.match("^\s*$")) {
             continue;
         }
-        opcodes.push(this.compileLine(line));
+        instructions.push(this.compileInstruction(line));
     }
-    return opcodes;
+    return instructions;
 };
 
-// Addressing
-
+// Addressing modes.
 Mars.prototype.modes = {
     "": function (address) { return address.value; },
     "#": function (address) { throw "immediate mode cannot be used in addressing"; },
@@ -102,13 +134,18 @@ Mars.prototype.modes = {
     "<": function (address) { return address.value + this.follow(address, "").b.value - 1; },
 };
 
+// Return symbols of addressing modes.
 Mars.prototype.getModes = function () {
     return this.modes.getOwnProperties();
 };
 
-Mars.prototype.resolve = function (address, mode) {
-    if (mode === undefined) {
+// Resolve a MARS address to an offset that it points to.
+Mars.prototype.resolve = function (address, forceMode) {
+    var mode;
+    if (forceMode === undefined) {
         mode = address.mode;
+    } else {
+        mode = forceMode;
     }
     if (!this.modes.hasOwnProperty(mode)) {
         throw mode + " is not a valid addressing mode";
@@ -117,12 +154,19 @@ Mars.prototype.resolve = function (address, mode) {
     return (this.context.ip + offset) % this.core.length;
 };
 
+// Resolve a MARS address to an instruction that it points to.
 Mars.prototype.follow = function (address, mode) {
     return this.core[this.resolve(address, mode)];
 };
 
-// Instructions
-
+// MARS opcodes. They are executed by stepProcess. The arguments are
+// arguments A and B of the executed instruction.
+//
+// The meaning of return values:
+//   null - terminate the current thread
+//   undefined - increase the instruction pointer by one
+//   an integer n - increase the instruction pointer by n
+//   a MARS address - set instruction pointer to a given address
 Mars.prototype.opcodes = {
     "DAT": function () {
         return null;
@@ -222,22 +266,24 @@ Mars.prototype.opcodes = {
     },
 };
 
+// Return names of opcodes.
 Mars.prototype.getOpcodes = function () {
     return this.opcodes.getOwnProperties();
 };
 
-Mars.prototype.processStep = function (process) {
+// Run a single instruction of a process.
+Mars.prototype.stepProcess = function (process) {
     this.context = {
         ip: process.queue.shift(),
         fork: null
     };
-    var cell = this.core[this.context.ip % this.core.length];
+    var instruction = this.core[this.context.ip % this.core.length];
 
-    if (!this.opcodes.hasOwnProperty(cell.opcode)) {
-        throw "instruction " + cell.opcode + " is not implemented";
+    if (!this.opcodes.hasOwnProperty(instruction.opcode)) {
+        throw "instruction " + instruction.opcode + " is not implemented";
     }
 
-    var offset = this.opcodes[cell.opcode].apply(this, [ cell.a, cell.b ]);
+    var offset = this.opcodes[instruction.opcode].apply(this, [ instruction.a, instruction.b ]);
 
     if (offset === null) {
         delete this.context;
@@ -257,61 +303,85 @@ Mars.prototype.processStep = function (process) {
     delete this.context;
 };
 
-Mars.prototype.isProcessIp = function (address) {
-    address = address % this.core.length;
-    for (var i = 0; i < this.processes.length; i++) {
-        var process = this.processes[i];
-        if (process.queue.indexOf(address) !== -1) {
-            return true;
-        }
-    }
-    return false;
-};
-
-Mars.prototype.spawn = function (opcodes) {
-    if (!opcodes.length) {
-        return;
-    }
-    var address = Math.floor(Math.random() * this.core.length);
-    for (var i = 0; i < opcodes.length; i++) {
-        this.core[(address + i) % this.core.length] = opcodes[i].clone();
-    }
-    this.processes.push(new Process([address]));
-};
-
+// Create a new thread of the current process.
 Mars.prototype.fork = function(address) {
     this.context.fork = this.resolve(address);
 };
 
-
-// Execution
-
-Mars.prototype.step = function () {
-    var process;
-
-    while (this.processes.length) {
-        process = this.processes.shift();
-        if (process.queue.length) {
-            break;
+// Return a process running at an offset.
+Mars.prototype.getProcessAt = function (offset) {
+    offset = offset % this.core.length;
+    for (var i = 0; i < this.processes.length; i++) {
+        var process = this.processes[i];
+        if (process.hasThreadAt(offset)) {
+            return process;
         }
     }
+};
 
-    if (process !== undefined) {
-        this.processStep(process);
-        if (process.queue.length) {
-            this.processes.push(process);
+// Return random offset for a given process or undefined when out of memory.
+Mars.prototype.getRandomProcessOffset = function (instructions) {
+    function hasEnoughSpaceAt(offset) {
+        var dat00 = new Instruction(
+            "DAT",
+            new Address("", 0),
+            new Address("", 0)
+        );
+        for (var i = offset; i < offset + instructions.length; i++) {
+            if (!this.core[i % this.core.length].isEqual(dat00)) {
+                return false;
+            }
         }
+        return true;
     }
 
-    return this.processes.length > 0;
+    var random = Math.floor(Math.random() * this.core.length);
+    for (var i = 0; i < this.core.length; i++) {
+        var offset = (random + i) % this.core.length;
+        if (hasEnoughSpaceAt.apply(this, [offset])) {
+            return offset;
+        }
+    }
+};
+
+// Spawn a process.
+Mars.prototype.spawn = function (owner, instructions, offset) {
+    if (offset === undefined) {
+        offset = this.getRandomProcessOffset(instructions);
+    }
+    if (offset === undefined) {
+        throw "out of memory";
+    }
+    var process = new Process(owner, [offset]);
+    for (var i = 0; i < instructions.length; i++) {
+        this.core[(offset + i) % this.core.length] = instructions[i].clone();
+    }
+    this.processes.push(process);
 };
 
 
-function MarsDisplay(mars, containerId, previewId, width, height) {
+// Run a single instruction of the next process in the queue.
+Mars.prototype.step = function () {
+    if (!this.processes.length) {
+        return false;
+    }
+    var process = this.processes.shift();
+    this.stepProcess(process);
+    if (process.queue.length) {
+        this.processes.push(process);
+    }
+    return true;
+};
+
+
+// Display a MARS in an HTML table.
+function MarsDisplay(mars, containerId, previewId, width, height, colors) {
     this.mars = mars;
     this.width = width;
     this.height = height;
-    this.cells = [];
+    this.tds = [];
+    this.colors = colors || {};
+    this.programColors = [ "#773F9E", "#B0CE21", "#C426D1", "#EF1E40", "#FFCC00", "#FF3300" ];
 
     var table = document.createElement("TABLE");
     table.className = "mars";
@@ -335,32 +405,49 @@ function MarsDisplay(mars, containerId, previewId, width, height) {
         for (var i = 0; i < width; i++) {
             var td = document.createElement("TD");
             addEventHandlers(td, i, j);
-            this.cells.push(td);
+            this.tds.push(td);
             tr.appendChild(td);
         }
         table.appendChild(tr);
     }
 
-    this.updateAllCells();
+    this.updateAll();
 
     document.getElementById(containerId).appendChild(table);
 }
 
-MarsDisplay.prototype.updateAllCells = function () {
+// Update the display of all memory locations.
+MarsDisplay.prototype.updateAll = function () {
     for (var i = 0; i < this.width * this.height; i++) {
-        this.updateCell(i);
+        this.updateAt(i);
     }
 };
 
-MarsDisplay.prototype.updateCell = function (address) {
-    className = "mars-cell ";
-
-    if (this.mars.core[address].opcode === "DAT") {
-        className += "mars-dat";
+// Generate a program color.
+MarsDisplay.prototype.generateProgramColor = function () {
+    if (this.programColors.length > 1) {
+        return this.programColors.shift();
+    } else {
+        return this.programColors[0];
     }
-    if (this.mars.isProcessIp(address)) {
-        className += " mars-program";
+};
+
+// Update the display of a memory location.
+MarsDisplay.prototype.updateAt = function (offset) {
+    var td = this.tds[offset];
+
+    td.style.backgroundColor = "";
+
+    td.className = "mars-cell ";
+    if (this.mars.core[offset].opcode === "DAT") {
+        td.className += "mars-dat";
     }
 
-    this.cells[address].className = className;
+    var process = this.mars.getProcessAt(offset);
+    if (process !== undefined) {
+        if (!this.colors.hasOwnProperty(process.owner)) {
+            this.colors[process.owner] = this.generateProgramColor();
+        }
+        td.style.backgroundColor = this.colors[process.owner];
+    }
 };
